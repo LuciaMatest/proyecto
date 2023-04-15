85adc2f2a7fef39717e22e0f76dbf0694e9f9561
Lucia <lucia.matest@educa.jcyl.es>
2023-04-15 11:15:59 +0000

    1

--- 1_PHP/config/portafolio.sql
+++ 1_PHP/config/portafolio.sql
@@ -13,7 +13,10 @@ CREATE TABLE `producto` (
   `nombre_producto` varchar(255) NOT NULL,
   `descripcion_producto` varchar(255) NOT NULL,
   `imagen_producto` varchar(255) NOT NULL,
-  `categoria_id` int
+  `precio` float NOT NULL,
+  `cantidad` int NOT NULL,
+  `categoria_id` int,
+  `proyecto_id` int
 );
 
 -- TRABAJOS REALIZADOS
@@ -21,9 +24,8 @@ CREATE TABLE `proyecto` (
   `id_proyecto` int PRIMARY KEY AUTO_INCREMENT,
   `nombre_proyecto` varchar(255) NOT NULL,
   `fecha_proyecto` datetime NOT NULL,
-  `producto_id` int,
-  `archivo_id` int,
-  `usuario_id` int
+  `usuario_id` int,
+  `factura_id` int
 );
 
 -- DOCUMENTOS QUE COMPARTIR CON EL CLIENTE
@@ -31,7 +33,8 @@ CREATE TABLE `archivo` (
   `id_archivo` int PRIMARY KEY AUTO_INCREMENT,
   `nombre_archivo` varchar(255) NOT NULL,
   `url_archivo` varchar(255) NOT NULL,
-  `descripcion_archivo` varchar(255) NOT NULL
+  `descripcion_archivo` varchar(255) NOT NULL,
+  `proyecto_id` int
 );
 
 -- USUARIO
@@ -41,6 +44,7 @@ CREATE TABLE `usuario` (
   `telefono_usuario` int NOT NULL,
   `email_usuario` varchar(255) NOT NULL,
   `contrasena_usuario` varchar(255) UNIQUE NOT NULL,
+  `borrado_usuario` boolean NOT NULL,
   `tipo_usuario` ENUM ('usuario', 'admin') NOT NULL
 );
 
@@ -59,33 +63,20 @@ CREATE TABLE `factura` (
   `nombre_factura` varchar(255) NOT NULL,
   `fecha_pago` datetime NOT NULL,
   `fecha_factura` datetime NOT NULL,
-  `estado` ENUM ('pendiente', 'pagado') NOT NULL,
-  `proyecto_id` int,
-  `detalles_id` int
-);
-
--- DETALLES DE LA FACTURA
-CREATE TABLE `detalles` (
-  `id_detalles` int PRIMARY KEY AUTO_INCREMENT,
-  `descripcion_detalles` varchar(255) NOT NULL,
-  `precio` float NOT NULL,
-  `IVA` int NOT NULL,
-  `cantidad` int NOT NULL
+  `estado` ENUM ('pendiente', 'pagado') NOT NULL
 );
 
 ALTER TABLE `producto` ADD FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id_categoria`);
 
-ALTER TABLE `proyecto` ADD FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id_producto`);
-
-ALTER TABLE `proyecto` ADD FOREIGN KEY (`archivo_id`) REFERENCES `archivo` (`id_archivo`);
+ALTER TABLE `producto` ADD FOREIGN KEY (`proyecto_id`) REFERENCES `proyecto` (`id_proyecto`);
 
 ALTER TABLE `proyecto` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id_usuario`);
 
+ALTER TABLE `proyecto` ADD FOREIGN KEY (`factura_id`) REFERENCES `factura` (`id_factura`);
+
+ALTER TABLE `archivo` ADD FOREIGN KEY (`proyecto_id`) REFERENCES `proyecto` (`id_proyecto`);
+
 ALTER TABLE `mensajes` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id_usuario`);
-
-ALTER TABLE `factura` ADD FOREIGN KEY (`proyecto_id`) REFERENCES `proyecto` (`id_proyecto`);
-
-ALTER TABLE `factura` ADD FOREIGN KEY (`detalles_id`) REFERENCES `detalles` (`id_detalles`);
 
 INSERT INTO categoria (nombre_categoria) VALUES('Ilustraciones');
 INSERT INTO categoria (nombre_categoria) VALUES('Diseño');
