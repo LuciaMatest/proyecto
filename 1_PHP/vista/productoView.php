38c938e938c49f97103da89e664c1d338f99fe77
Lucia <lucia.matest@educa.jcyl.es>
2023-04-16 21:47:05 +0000

    1

--- 1_PHP/vista/productoView.php
+++ 1_PHP/vista/productoView.php
@@ -20,6 +20,30 @@
 </div>
 
 <div class="productDesc container my-5">
+    <section class="row row-cols-1 my-4">
+        <div class="col">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                    <svg class="bd-placeholder-img" width="700" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                    </svg>
+                </div>
+                <div class="textoProd col p-4 px-5 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary">World</strong>
+                    <h3 class="mb-0">Featured post</h3>
+                    <div class="mb-1 text-muted">Nov 12</div>
+                    <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to
+                        additional content.Multiple lines of text that form the lede, informing new readers quickly and efficiently
+                        about what’s most interesting in this post’s contents.
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content.Multiple lines of text that form the lede, informing new readers quickly and efficiently
+                        about what’s most interesting in this post’s contents.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="row">
         <div class="col-md-8">
             <div class="row featurette">
@@ -155,6 +179,7 @@
             </div>
         </div>
     </section>
+
     <section class="row row-cols-1 mt-4">
         <div class="col">
             <div class="card shadow-sm">
