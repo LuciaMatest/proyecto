b5f8517126115e8d57737b56a1c35cba33a7a1f3
LuciaMatest <114143383+LuciaMatest@users.noreply.github.com>
2023-04-20 12:41:14 +0200

    1

--- 1_PHP/vista/privada.php
+++ 1_PHP/vista/privada.php
@@ -269,118 +269,219 @@
 </section>
 
 <section id="chat">
-    <div class="container py-5">
+<div class="container py-5">
 
-    <div class="row">
+<div class="row">
 
-      <div class="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
+  <div class="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
 
-        <h5 class="font-weight-bold mb-3 text-center text-lg-start">Clientes</h5>
+    <h5 class="font-weight-bold mb-3 text-center text-lg-start">Clientes</h5>
 
-        <div class="card">
-          <div class="card-body">
+    <div class="card">
+      <div class="card-body">
 
-            <ul class="list-unstyled mb-0">
-              <li class="mb-1">
-                <div class="d-flex flex-row">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar" class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
-                  <div class="pt-1">
-                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
-                      John Doe
-                    </button>
+        <ul class="list-unstyled mb-0">
+          <li class="my-2">
+            <div class="container">
+              <div class="row align-items-center justify-content-between">
+                <div class="col-auto">
+                  <img src="https://via.placeholder.com/150" alt="Imagen de ejemplo" class="rounded-circle" width="50">
+                  <span class="ms-3 fw-bold" style="font-size: 17px;">John Doe</span>
+                </div>
+                <div class="col-auto">
+                  <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">Editar</button>
+                  <button class="btn btn-toggle align-items-center rounded collapsed">Borrar</button>
+                </div>
+              </div>
+            </div>
+
+            <div class="collapse" id="home-collapse" style="">
+              <div class="container mt-4">
+                <form>
+                  <div class="mb-2">
+                    <input type="text" class="form-control" id="username" placeholder="Nombre de usuario">
                   </div>
-                </div>
-
-                <div class="collapse" id="home-collapse" style="">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Overview</a></li>
-                    <li><a href="#" class="link-dark rounded">Updates</a></li>
-                    <li><a href="#" class="link-dark rounded">Reports</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li class="mb-1">
-                <div class="d-flex flex-row">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar" class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
-                  <div class="pt-1">
-                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse2" aria-expanded="false">
-                      John Doe
-                    </button>
+                  <div class="mb-2">
+                    <input type="email" class="form-control" id="email" placeholder="Email">
                   </div>
+                  <div class="mb-2">
+                    <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                  </div>
+                  <div class="mb-2">
+                    <input type="password" class="form-control" id="confirmPassword" placeholder="Confirme su contraseña">
+                  </div>
+                  <div class="d-flex justify-content-between">
+                    <button class="btn btn-toggle align-items-center rounded collapsed">Guardar cambios</button>
+                    <button class="btn btn-toggle align-items-center rounded collapsed">Cancelar</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </li>
+          <li class="my-2">
+            <div class="container">
+              <div class="row align-items-center justify-content-between">
+                <div class="col-auto">
+                  <img src="https://via.placeholder.com/150" alt="Imagen de ejemplo" class="rounded-circle" width="50">
+                  <span class="ms-3 fw-bold" style="font-size: 17px;">John Doe</span>
                 </div>
-
-                <div class="collapse" id="home-collapse2" style="">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Overview</a></li>
-                    <li><a href="#" class="link-dark rounded">Updates</a></li>
-                    <li><a href="#" class="link-dark rounded">Reports</a></li>
-                  </ul>
+                <div class="col-auto">
+                  <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">Editar</button>
+                  <button class="btn btn-toggle align-items-center rounded collapsed">Borrar</button>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+              </div>
+            </div>
 
-      <div class="col-md-6 col-lg-7 col-xl-8">
-        <h5 class="font-weight-bold mb-3 text-center text-lg-start">Chat</h5>
-        <ul class="list-unstyled">
-          <li class="d-flex justify-content-between mb-4">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar" class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
-            <div class="card">
-              <div class="card-header d-flex justify-content-between p-3">
-                <p class="fw-bold mb-0">Brad Pitt</p>
-                <p class="text-muted small mb-0"><i class="far fa-clock"></i> 12 mins ago</p>
-              </div>
-              <div class="card-body">
-                <p class="mb-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                  labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex justify-content-between mb-4">
-            <div class="card w-100">
-              <div class="card-header d-flex justify-content-between p-3">
-                <p class="fw-bold mb-0">Lara Croft</p>
-                <p class="text-muted small mb-0"><i class="far fa-clock"></i> 13 mins ago</p>
-              </div>
-              <div class="card-body">
-                <p class="mb-0">
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                  laudantium.
-                </p>
-              </div>
-            </div>
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar" class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
-          </li>
-          <li class="d-flex justify-content-between mb-4">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar" class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
-            <div class="card">
-              <div class="card-header d-flex justify-content-between p-3">
-                <p class="fw-bold mb-0">Brad Pitt</p>
-                <p class="text-muted small mb-0"><i class="far fa-clock"></i> 10 mins ago</p>
-              </div>
-              <div class="card-body">
-                <p class="mb-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                  labore et dolore magna aliqua.
-                </p>
+            <div class="collapse" id="home-collapse" style="">
+              <div class="container mt-4">
+                <form>
+                  <div class="mb-2">
+                    <input type="text" class="form-control" id="username" placeholder="Nombre de usuario">
+                  </div>
+                  <div class="mb-2">
+                    <input type="email" class="form-control" id="email" placeholder="Email">
+                  </div>
+                  <div class="mb-2">
+                    <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                  </div>
+                  <div class="mb-2">
+                    <input type="password" class="form-control" id="confirmPassword" placeholder="Confirme su contraseña">
+                  </div>
+                  <div class="d-flex justify-content-between">
+                    <button class="btn btn-toggle align-items-center rounded collapsed">Guardar cambios</button>
+                    <button class="btn btn-toggle align-items-center rounded collapsed">Cancelar</button>
+                  </div>
+                </form>
               </div>
             </div>
           </li>
-          <form class="mt-3">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Escribe un mensaje...">
-              <button class="btn btn-primary" type="submit">Enviar</button>
+          <li class="my-2">
+            <div class="container">
+              <div class="row align-items-center justify-content-between">
+                <div class="col-auto">
+                  <img src="https://via.placeholder.com/150" alt="Imagen de ejemplo" class="rounded-circle" width="50">
+                  <span class="ms-3 fw-bold" style="font-size: 17px;">John Doe</span>
+                </div>
+                <div class="col-auto">
+                  <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">Editar</button>
+                  <button class="btn btn-toggle align-items-center rounded collapsed">Borrar</button>
+                </div>
+              </div>
             </div>
-          </form>
+
+            <div class="collapse" id="home-collapse" style="">
+              <div class="container mt-4">
+                <form>
+                  <div class="mb-2">
+                    <input type="text" class="form-control" id="username" placeholder="Nombre de usuario">
+                  </div>
+                  <div class="mb-2">
+                    <input type="email" class="form-control" id="email" placeholder="Email">
+                  </div>
+                  <div class="mb-2">
+                    <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                  </div>
+                  <div class="mb-2">
+                    <input type="password" class="form-control" id="confirmPassword" placeholder="Confirme su contraseña">
+                  </div>
+                  <div class="d-flex justify-content-between">
+                    <button class="btn btn-toggle align-items-center rounded collapsed">Guardar cambios</button>
+                    <button class="btn btn-toggle align-items-center rounded collapsed">Cancelar</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </li>
         </ul>
-
       </div>
-
     </div>
-
   </div>
+
+  <div class="col-md-6 col-lg-7 col-xl-8">
+    <h5 class="font-weight-bold mb-3 text-center text-lg-start">Chat</h5>
+    <div class="chat-container d-flex flex-column" style="min-height: 400px;">
+      <ul class="list-unstyled">
+        <li class="d-flex justify-content-between mb-4">
+          <img src="https://via.placeholder.com/150" alt="avatar" class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
+          <div class="card">
+            <div class="card-header d-flex justify-content-between p-3">
+              <p class="fw-bold mb-0">John Doe</p>
+            </div>
+            <div class="card-body">
+              <p class="mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between mb-4">
+          <div class="card w-100">
+            <div class="card-header d-flex justify-content-between p-3">
+              <p class="fw-bold mb-0">Lulú</p>
+            </div>
+            <div class="card-body">
+              <p class="mb-0">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                laudantium.
+              </p>
+            </div>
+          </div>
+          <img src="https://via.placeholder.com/150" alt="avatar" class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
+        </li>
+        <li class="d-flex justify-content-between mb-4">
+          <img src="https://via.placeholder.com/150" alt="avatar" class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
+          <div class="card">
+            <div class="card-header d-flex justify-content-between p-3">
+              <p class="fw-bold mb-0">John Doe</p>
+            </div>
+            <div class="card-body">
+              <p class="mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between mb-4">
+          <div class="card w-100">
+            <div class="card-header d-flex justify-content-between p-3">
+              <p class="fw-bold mb-0">Lulú</p>
+            </div>
+            <div class="card-body">
+              <p class="mb-0">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                laudantium.
+              </p>
+            </div>
+          </div>
+          <img src="https://via.placeholder.com/150" alt="avatar" class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
+        </li>
+        <li class="d-flex justify-content-between mb-4">
+          <img src="https://via.placeholder.com/150" alt="avatar" class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
+          <div class="card">
+            <div class="card-header d-flex justify-content-between p-3">
+              <p class="fw-bold mb-0">John Doe</p>
+            </div>
+            <div class="card-body">
+              <p class="mb-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <form>
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Escribe un mensaje...">
+          <button class="btn btn-primary" type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</div>
+
+</div>
 </section>
