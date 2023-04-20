fe601aefa1a8831a1a9c71a9d829fb9e60b326d0
Lucia <lucia.matest@educa.jcyl.es>
2023-04-20 20:14:39 +0000

    1

--- 1_PHP/controlador/HomeController.php
+++ 1_PHP/controlador/HomeController.php
@@ -3,10 +3,12 @@ if (isset($_REQUEST['ver'])) {
     $_SESSION['controlador'] = $controladores['proyecto'];
     $_SESSION['pagina'] = 'Proyectos';
     $_SESSION['vista'] = $vistas['proyecto'];
+    $_SESSION['previous_page'] = $_SERVER['REQUEST_URI'];
     require_once $_SESSION['controlador'];
 } elseif (isset($_REQUEST['contacto'])) {
     $_SESSION['controlador'] = $controladores['contacto'];
     $_SESSION['pagina'] = 'Contacto';
     $_SESSION['vista'] = $vistas['contacto'];
+    $_SESSION['previous_page'] = $_SERVER['REQUEST_URI'];
     require_once $_SESSION['controlador'];
 }
