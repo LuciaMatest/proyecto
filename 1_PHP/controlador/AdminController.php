d3988c36e5088c34895ed483c87b00aeceae634b
Lucia <lucia.matest@educa.jcyl.es>
2023-04-28 18:10:47 +0000

    1

--- 1_PHP/controlador/AdminController.php
+++ 1_PHP/controlador/AdminController.php
@@ -4,10 +4,6 @@ if (isset($_REQUEST['volver'])) {
     $_SESSION['pagina'] = 'Home';
     $_SESSION['vista'] = $vistas['home'];
     require_once $_SESSION['controlador'];
-} elseif (isset($_REQUEST['enviarMensajesAdmin'])) {
-    $messages = ChatDAO::findAll();
-    $nuevoMessage = new Chat(null, $_REQUEST['descripcion_mensaje'], date('Y-m-d'), $_REQUEST['usuario_id']);
-    ChatDAO::insert($nuevoMessage);
 } else {
     $messages = ChatDAO::findAll();
 }
