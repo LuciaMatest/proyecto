da8105e26f8012d2d25c6937112f88736cfc8be6
Lucia <lucia.matest@educa.jcyl.es>
2023-05-02 21:25:41 +0000

    1

--- 1_PHP/vista/adminView.php
+++ 1_PHP/vista/adminView.php
@@ -229,9 +229,7 @@
                           <span class="ms-3 fw-bold" style="font-size: 15px;"><?php echo $usuario->nombre_usuario; ?></span>
                         </div>
                         <div class="col-auto">
-                          <form action="./index.php" method="post">
-                            <button type="submit" class="transparent-button" name="verChat" id="verChat" data-id="<?php echo $usuario->id_usuario; ?>" value="<?php echo $usuario->id_usuario; ?>"><i class="bi bi-chat-dots"></i></button>
-                          </form>
+                          <button type="submit" class="transparent-button" name="verChat" id="verChat" data-id="<?php echo $usuario->id_usuario; ?>" value="<?php echo $usuario->id_usuario; ?>"><i class="bi bi-chat-dots"></i></button>
                           <button type="submit" class="transparent-button" name="editarUsuario" id="editarUsuario" data-bs-toggle="collapse" data-bs-target="#<?php echo $usuario->id_usuario; ?>" aria-expanded="false"><i class="bi bi-pencil-square me-2"></i></button>
                           <button type="submit" class="transparent-button" name="borrarUsuario" id="borrarUsuario"><i class="bi bi-trash3"></i></button>
                         </div>
@@ -315,26 +313,25 @@
       <div class="col-md-6 col-lg-7 col-xl-8">
         <h5 class="font-weight-bold mb-3 text-center text-lg-start">Chat</h5>
         <div class="chat-container d-flex flex-column" style="min-height: 400px;">
-          <form action="./index.php" method="post">
-            <?php
-            if (isset($_POST['verChat'])) {
-              $id_usuario_actual = $_SESSION['id_usuario'];
-              $id_usuario_boton = $_POST['verChat'];
-            
-              if ($id_usuario_actual == $id_usuario_boton) {
-                require_once('./config/conexion.php');
-                // Transacción
-                try {
-                  $conexion = mysqli_connect($_SERVER['SERVER_ADDR'], USER, PASS, BBDD);
-                  $sql = "SELECT mensaje.*, usuario.nombre_usuario FROM mensaje JOIN usuario ON mensaje.id_usuario_envia = usuario.id_usuario WHERE (mensaje.id_usuario_envia = $id_usuario_boton OR mensaje.id_usuario_recibe = $id_usuario_boton) AND (mensaje.id_usuario_envia = $id_usuario_actual OR mensaje.id_usuario_recibe = $id_usuario_actual) ORDER BY fecha_mensaje ASC;";
-                  $resultado = mysqli_query($conexion, $sql);
-                  // Mostrar mensajes
-                  if ($resultado->num_rows > 0) {
-                    echo '<ul class="list-unstyled">';
-                    while ($message = $resultado->fetch_object()) {
-                      $nombreUsuario = $message->nombre_usuario;
-                      if ($nombreUsuario === 'Lulú') {
-                        echo '<li class="d-flex justify-content-between mb-4">
+          <?php
+          if (isset($_REQUEST['verChat'])) {
+            $id_usuario_actual = $_SESSION['id_usuario'];
+            $id_usuario_boton = $_POST['verChat'];
+
+            if ($id_usuario_actual == $id_usuario_boton) {
+              require_once('./config/conexion.php');
+              // Transacción
+              try {
+                $conexion = mysqli_connect($_SERVER['SERVER_ADDR'], USER, PASS, BBDD);
+                $sql = "SELECT mensaje.*, usuario.nombre_usuario FROM mensaje JOIN usuario ON mensaje.id_usuario_envia = usuario.id_usuario WHERE (mensaje.id_usuario_envia = $id_usuario_boton OR mensaje.id_usuario_recibe = $id_usuario_boton) AND (mensaje.id_usuario_envia = $id_usuario_actual OR mensaje.id_usuario_recibe = $id_usuario_actual) ORDER BY fecha_mensaje ASC;";
+                $resultado = mysqli_query($conexion, $sql);
+                // Mostrar mensajes
+                if ($resultado->num_rows > 0) {
+                  echo '<ul class="list-unstyled">';
+                  while ($message = $resultado->fetch_object()) {
+                    $nombreUsuario = $message->nombre_usuario;
+                    if ($nombreUsuario === 'Lulú') {
+                      echo '<li class="d-flex justify-content-between mb-4">
                                 <div class="card w-100">
                                 <div class="card-header d-flex justify-content-between p-3">
                                 <p class="fw-bold mb-0">' . $nombreUsuario . '</p>
@@ -345,8 +342,8 @@
                                 </div>
                                 <img src="./webroot/recursos/perfil/perfil2.png" alt="avatar" class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">
                               </li>';
-                      } else {
-                        echo '<li class="d-flex justify-content-between mb-4">
+                    } else {
+                      echo '<li class="d-flex justify-content-between mb-4">
                               <img src="./webroot/recursos/perfil/perfil.png" alt="avatar" class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60">
                                 <div class="card w-100">
                                   <div class="card-header d-flex justify-content-between p-3">
@@ -357,30 +354,32 @@
                                   </div>
                                 </div>
                               </li>';
-                      }
                     }
-                    echo '</ul>';
-                  } else {
-                    echo "<p>No hay mensajes.</p>";
                   }
-                  // Cerrar conexión
-                  mysqli_close($conexion);
-                } catch (Exception $ex) {
-                  if ($ex->getCode() == 2002) {
-                    echo '<span style="color:brown"> Fallo de conexión </span>';
-                  }
-                  if ($ex->getCode() == 1049) {
-                    echo '<span style="color:brown"> Base de datos desconocida </span>';
-                  }
-                  if ($ex->getCode() == 1045) {
-                    echo '<span style="color:brown"> Datos incorrectos </span>';
-                  }
+                  echo '</ul>';
+                } else {
+                  echo "<p>No hay mensajes.</p>";
+                }
+                // Cerrar conexión
+                mysqli_close($conexion);
+              } catch (Exception $ex) {
+                if ($ex->getCode() == 2002) {
+                  echo '<span style="color:brown"> Fallo de conexión </span>';
+                }
+                if ($ex->getCode() == 1049) {
+                  echo '<span style="color:brown"> Base de datos desconocida </span>';
+                }
+                if ($ex->getCode() == 1045) {
+                  echo '<span style="color:brown"> Datos incorrectos </span>';
                 }
               }
             }
-            ?>
+          }
+          ?>
+
+          <form method="post">
             <div class="input-group">
-              <input type="hidden" name="id_usuario" value="<?php echo $usuario->id_usuario; ?>">
+              <!-- <input type="hidden" name="id_usuario" value="<?php //echo $usuario->id_usuario; ?>"> -->
               <input type="text" class="form-control" id="messageInput" name="descripcion_mensaje" placeholder="Escribe un mensaje...">
               <input type="submit" class="btn btn-primary" id="sendMessageBtn" name="enviarMensajesUser" value="Enviar">
             </div>
