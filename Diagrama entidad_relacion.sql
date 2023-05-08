ef54ccb097bf0d16a006ddc29c7ef1a3ef6bc714
LuciaMatest <114143383+LuciaMatest@users.noreply.github.com>
2023-05-08 10:33:34 +0200

    1

---
+++ Diagrama entidad_relacion.sql
@@ -0,0 +1,68 @@
+CREATE TABLE `categoria` (
+  `id_categoria` int PRIMARY KEY AUTO_INCREMENT,
+  `nombre_categoria` varchar(255) NOT NULL
+);
+
+CREATE TABLE `producto` (
+  `id_producto` int PRIMARY KEY AUTO_INCREMENT,
+  `nombre_producto` varchar(255) NOT NULL,
+  `descripcion_producto` varchar(255) NOT NULL,
+  `precio` float NOT NULL,
+  `cantidad` int NOT NULL,
+  `categoria_id` int,
+  `proyecto_id` int,
+  `imagen_id` int
+);
+
+CREATE TABLE `imagen` (
+  `id_imagen` int PRIMARY KEY AUTO_INCREMENT,
+  `url_imagen` varchar(255) NOT NULL
+);
+
+CREATE TABLE `proyecto` (
+  `id_proyecto` int PRIMARY KEY AUTO_INCREMENT,
+  `nombre_proyecto` varchar(255) NOT NULL,
+  `fecha_proyecto` datetime NOT NULL,
+  `usuario_id` int,
+  `factura_id` int
+);
+
+CREATE TABLE `usuario` (
+  `id_usuario` int PRIMARY KEY AUTO_INCREMENT,
+  `nombre_usuario` varchar(255) NOT NULL,
+  `telefono_usuario` int NOT NULL,
+  `email_usuario` varchar(255) NOT NULL,
+  `contrasena_usuario` varchar(255) UNIQUE NOT NULL,
+  `borrado_usuario` boolean NOT NULL DEFAULT (0),
+  `tipo_usuario` ENUM ('usuario', 'admin') NOT NULL DEFAULT (usuario)
+);
+
+CREATE TABLE `mensaje` (
+  `id_mensajes` int PRIMARY KEY AUTO_INCREMENT,
+  `descripcion_mensajes` varchar(255) NOT NULL,
+  `fecha_mensajes` datetime,
+  `id_usuario_envia` int,
+  `id_usuario_recibe` int
+);
+
+CREATE TABLE `factura` (
+  `id_factura` int PRIMARY KEY AUTO_INCREMENT,
+  `nombre_factura` varchar(255) NOT NULL,
+  `fecha_pago` datetime NOT NULL,
+  `fecha_factura` datetime NOT NULL,
+  `estado` ENUM ('pendiente', 'pagado') NOT NULL
+);
+
+ALTER TABLE `producto` ADD FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id_categoria`);
+
+ALTER TABLE `producto` ADD FOREIGN KEY (`proyecto_id`) REFERENCES `proyecto` (`id_proyecto`);
+
+ALTER TABLE `producto` ADD FOREIGN KEY (`imagen_id`) REFERENCES `imagen` (`id_imagen`);
+
+ALTER TABLE `proyecto` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id_usuario`);
+
+ALTER TABLE `proyecto` ADD FOREIGN KEY (`factura_id`) REFERENCES `factura` (`id_factura`);
+
+ALTER TABLE `mensaje` ADD FOREIGN KEY (`id_usuario_envia`) REFERENCES `usuario` (`id_usuario`);
+
+ALTER TABLE `mensaje` ADD FOREIGN KEY (`id_usuario_recibe`) REFERENCES `usuario` (`id_usuario`);
