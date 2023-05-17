d2349d34130688901b5ea21a256902021a652b3a
Lucia <lucia.matest@educa.jcyl.es>
2023-05-17 19:49:03 +0000

    1

--- 1_PHP/modelo/Usuario.php
+++ 1_PHP/modelo/Usuario.php
@@ -1,35 +1,34 @@
 <?php
-class Usuario
-{
-    private $id_usuario;
-    private $nombre_usuario;
-    private $telefono_usuario;
-    private $email_usuario;
-    private $contrasena_usuario;
-    private $borrado_usuario;
+    class Usuario{
+        private $id_usuario;
+        private $nombre_usuario;
+        private $telefono_usuario;
+        private $email_usuario;
+        private $contrasena_usuario;
+        private $borrado_usuario;
+        private $tipo_usuario;
 
-    public function __construct($nombre_usuario, $telefono_usuario, $email_usuario, $contrasena_usuario, $borrado_usuario)
-    {
-        $this->nombre_usuario = $nombre_usuario;
-        $this->telefono_usuario = $telefono_usuario;
-        $this->email_usuario = $email_usuario;
-        $this->contrasena_usuario = $contrasena_usuario;
-        $this->borrado_usuario = $borrado_usuario;
-    }
-
-    public function __get($get)
-    {
-        //Si la propiedad no existiera retornaría null
-        if (property_exists(__CLASS__, $get)) {
-            return $this->$get;
+        public function __construct($id_usuario,$nombre_usuario,$telefono_usuario,$email_usuario,$contrasena_usuario,$borrado_usuario,$tipo_usuario){
+            $this->id_usuario = $id_usuario;
+            $this->nombre_usuario = $nombre_usuario;
+            $this->telefono_usuario = $telefono_usuario;
+            $this->email_usuario = $email_usuario;
+            $this->contrasena_usuario = $contrasena_usuario;
+            $this->borrado_usuario = $borrado_usuario;
+            $this->tipo_usuario = $tipo_usuario;
         }
-        return null;
-    }
 
-    public function __set($set, $valor)
-    {
-        if (property_exists(__CLASS__, $set)) {
-            return $this->$set = $valor;
+        public function __get($get){
+            //Si la propiedad no existiera retornaría null
+            if (property_exists(__CLASS__,$get)) {
+                return $this->$get;
+            }
+            return null;
+        }
+    
+        public function __set($set,$valor){
+            if (property_exists(__CLASS__,$set)) {
+                return $this->$set=$valor;
+            }
         }
     }
-}
